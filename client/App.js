597e8ca3cecfce70e0c1aa2a934665644ace00ee
Jessy Jupiter <jessy@macbook-pro-de-jessy.home>
2022-02-27 12:22:06 +0100

    Add StyleGuide + Fonts

--- client/App.js
+++ client/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import LoadFonts from './components/layouts/LoadFonts'
+import StyleGuide from './components/utils/StyleGuide'
 
-export default function App() {
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoadFonts>
+      <Text style={StyleGuide.typography.text1}>Open up App.js to start working on your app!</Text>
+    </LoadFonts>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
