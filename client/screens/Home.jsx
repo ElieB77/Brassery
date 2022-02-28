0bb4b43e397ca022354dbb4b26f9f314e9390d83
GauvainThery <gauvain.thery@payfit.com>
2022-02-28 14:57:57 +0100

    Reset Home

--- client/screens/Home.jsx
+++ client/screens/Home.jsx
@@ -1,38 +1,16 @@
 import { View } from "react-native";
 
-import Recipe from "../components/recipeElements/recipe";
-import RecipeItem from "../components/recipeElements/recipeItem";
-import RecipeDescription from "../components/recipeElements/recipeDescription";
+import CustomButton from "../components/CustomButton";
 
-import StyleGuide from "../components/utils/StyleGuide";
-
-
-const Home = () => {
+const Home = ({ navigation }) => {
     return (
-        <View style={[StyleGuide.container,{alignItems:"center"}]}>
-            <Recipe>
-                <RecipeDescription title="Lore Ipsum" content="Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget 
-                    consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus.
-                    Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris 
-                    blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam 
-                    id dui posuere blandit."/>
-                <RecipeItem
-                    title="Hello"
-                    content="Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget 
-                    consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus.
-                    Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris 
-                    blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam 
-                    id dui posuere blandit."
-                />
-                <RecipeItem
-                    title="Hello"
-                    content="Sed porttitor lectus nibh. Vivamus magna justo, lacinia eget 
-                    consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus.
-                    Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris 
-                    blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur aliquet quam 
-                    id dui posuere blandit."
-                />
-            </Recipe>
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <CustomButton
+                title="Aller sur Login"
+                onPress={() => navigation.navigate("SignUp")}
+            />
         </View>
     );
 };
