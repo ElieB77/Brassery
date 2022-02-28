cd3d271b97a614d5a96f87052738fe6d27c453e7
GauvainThery <gauvain.thery@payfit.com>
2022-02-28 10:02:59 +0100

    test

--- client/App.js
+++ client/App.js
@@ -1,19 +1,19 @@
 import { Text, View } from "react-native";
 import LoadFonts from "./components/layouts/LoadFonts";
 import StyleGuide from "./components/utils/StyleGuide";
-import List from "./components/lists/list";
-import ListItem from "./components/lists/listItem";
+import Recipe from "./components/recipes/recipe";
+import RecipeItem from "./components/recipes/recipeItem";
 
 const App = () => {
     return (
         <LoadFonts
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-            <List>
-                <ListItem key={1} title="Hello" content="Lorem ipsum" />
-                <ListItem key={2} title="Hello" content="Lorem ipsum" />
-                <ListItem key={3} title="Hello" content="Lorem ipsum" />
-            </List>
+            <Recipe>
+                <RecipeItem key={1} title="Hello" content="Lorem ipsum" />
+                <RecipeItem key={2} title="Hello" content="Lorem ipsum" />
+                <RecipeItem key={3} title="Hello" content="Lorem ipsum" />
+            </Recipe>
         </LoadFonts>
     );
 };
