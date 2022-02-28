0b39618533377670e3feeacb6e432eb9a5da987e
Elie Bissor <elie@Elies-MacBook-Pro.local>
2022-02-28 11:13:20 +0100

    Initialization of input

--- client/App.js
+++ client/App.js
@@ -1,16 +1,19 @@
-import { Text, View } from 'react-native';
-import LoadFonts from './components/layouts/LoadFonts'
-import ProgressBar from './components/utils/ProgressBar';
-
+import { Text, View } from "react-native";
+import LoadFonts from "./components/layouts/LoadFonts";
+import ProgressBar from "./components/utils/ProgressBar";
+import InputText from "./components/utils/form-elements/Input";
+import CustomButton from "./components/CustomButton";
 
 const App = () => {
   return (
     <LoadFonts>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ProgressBar pourcent='45' />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <InputText type="password" />
+        <InputText type="text" />
+        <InputText type="textarea" />
       </View>
     </LoadFonts>
   );
-}
+};
 
-export default App
+export default App;
