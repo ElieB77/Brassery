bafebde1776a60336bbfcf074656921fa993eaea
Jessy Jupiter <jessy@MacBook-Pro-de-Jessy.local>
2022-02-28 12:03:22 +0100

    fix App.js

--- client/App.js
+++ client/App.js
@@ -1,12 +1,25 @@
-import { Text, View } from 'react-native';
 import LoadFonts from './components/layouts/LoadFonts'
-import StyleGuide from './components/utils/StyleGuide'
 
+import Home from './screens/Home'
+import SignUp from './screens/authentification/SignUp'
+
+import Navbar from './components/layouts/navbar/Navbar';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <LoadFonts>
-      <Text style={StyleGuide.typography.text1}>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Navbar" component={Navbar} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </LoadFonts>
   );
 }
