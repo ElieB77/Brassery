66a7875e6e808155912bf044d8f152a39bf39d04
Jessy Jupiter <jessy@MacBook-Pro-de-Jessy.local>
2022-03-01 09:38:04 +0100

    Fix StyleGuide

--- client/components/utils/StyleGuide.jsx
+++ client/components/utils/StyleGuide.jsx
@@ -69,16 +69,8 @@ const StyleGuide = {
     fontSize: 12,
     lineHeight: 16,
     fontFamily: 'Manrope_700Bold',
-
-  borderRadius: 8,
-  shadowProp: {
-    shadowColor: "rgba(0, 0, 0, 0.25)",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOffsetInput: { width: 0, height: 0 },
-    shadowOpacity: 0.4,
-    shadowRadius: 3,
-
   },
+
   borderRadius: 8,
   shadowProp: shadow,
 };
