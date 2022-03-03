8f7986c7a38649517dda77cb8b4ad1fd0384a2b8
GauvainThery <gauvain.thery@payfit.com>
2022-03-03 16:21:34 +0100

    Correct space

--- client/components/overlays/ingredientsOverlay.jsx
+++ client/components/overlays/ingredientsOverlay.jsx
@@ -31,7 +31,6 @@ export default function IngredientsOverlay({
                 return `Houblons`;
             case "cultures":
                 return `Levures`;
-
             case "fermentables":
                 return `Malts`;
             case "miscs":
