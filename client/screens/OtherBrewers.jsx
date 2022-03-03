1dc8afc7bf95fbcaaffb8f67e4af4c644a34e69f
Elie Bissor <elie@Elies-MacBook-Pro.local>
2022-03-03 15:32:42 +0100

    resource front end done

--- client/screens/OtherBrewers.jsx
+++ client/screens/OtherBrewers.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 const Location = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Autres Brasseurs</Text>
     </View>
   );
