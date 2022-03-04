4d15445adde0e2090bb04cf02843d7bc43a9b163
GauvainThery <gauvain.thery@payfit.com>
2022-03-04 13:55:18 +0100

    Set up for demo

--- client/screens/MyBrewery.jsx
+++ client/screens/MyBrewery.jsx
@@ -9,7 +9,7 @@ const Home = ({ navigation }) => {
             style={{ flex: 1, justifyContent: "space-around", alignItems: "center" }}
         >
             <CustomButton
-                title="Punk IPA - Batch 🧪"
+                title="Batch 🧪"
                 onPress={() =>
                     navigation.navigate("Batch", {
                         batchId: "6221fc885223412400e58d54",
@@ -17,7 +17,7 @@ const Home = ({ navigation }) => {
                 }
             />
             <CustomButton
-                title="Punk IPA - Recipe 🧪"
+                title="Recipe 🧪"
                 onPress={() =>
                     navigation.navigate("Recipe", {
                         recipeId: "6221fc055223412400e58d53",
