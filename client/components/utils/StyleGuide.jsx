50755d49b4dfd64ac74aa1a98adc1daddbcf89b2
GauvainThery <gauvain.thery@payfit.com>
2022-03-06 13:52:58 +0100

    Fixing running of the app by rollback

--- client/components/utils/StyleGuide.jsx
+++ client/components/utils/StyleGuide.jsx
@@ -11,76 +11,81 @@ const shadow =
         : { elevation: 10 };
 
 const StyleGuide = {
-  colors: {
-    primary: '#EBC56E',
-    secondary: '#435E75',
-    third: '#E6D8C1',
-    white: '#FFFDFB',
-    black: '#3C3C3B',
-    gray: '#C4C4C4',
-    green: '#56A754',
-    red: '#BB312C',
-    lowOpacity: 'rgba(235,187,110,0.3)',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFDFB',
-    paddingTop: 45,
-    paddingBottom: 45,
-    paddingLeft: 25,
-    paddingRight: 25,
-  },
-  typography: {
-    text1: {
-      fontSize: 20,
-      lineHeight: 27,
-      textTransform: 'uppercase',
-      fontFamily: 'Manrope_700Bold',
+    colors: {
+        primary: "#EBC56E",
+        secondary: "#435E75",
+        third: "#E6D8C1",
+        white: "#FFFDFB",
+        black: "#3C3C3B",
+        gray: "#C4C4C4",
+        green: "#56A754",
+        red: "#BB312C",
+        lowOpacity: "rgba(235,187,110,0.3)",
     },
-    text2: {
-      fontSize: 24,
-      lineHeight: 33,
-      fontFamily: 'Manrope_700Bold',
+    container: {
+        flex: 1,
+        backgroundColor: "#FFFDFB",
+        paddingTop: 45,
+        paddingBottom: 45,
+        paddingLeft: 25,
+        paddingRight: 25,
     },
-    text3: {
-      fontSize: 12,
-      lineHeight: 16,
-      fontFamily: 'Manrope_700Bold',
+    typography: {
+        text1: {
+            fontSize: 20,
+            lineHeight: 27,
+            textTransform: "uppercase",
+            fontFamily: "Manrope_700Bold",
+        },
+        text2: {
+            fontSize: 24,
+            lineHeight: 33,
+            fontFamily: "Manrope_700Bold",
+        },
+        text3: {
+            fontSize: 12,
+            lineHeight: 16,
+            fontFamily: "Manrope_700Bold",
+        },
+        text4: {
+            fontSize: 10,
+            lineHeight: 14,
+            fontFamily: "Manrope_700Bold",
+        },
+        text5: {
+            fontSize: 18,
+            lineHeight: 30,
+            fontFamily: "Manrope_700Bold",
+        },
+        overline: {
+            fontSize: 14,
+            lineHeight: 19,
+            fontFamily: "Manrope_700Bold",
+        },
+        linkText: {
+            textDecorationLine: "underline",
+        },
+        textButton: {
+            fontSize: 12,
+            lineHeight: 16,
+            fontFamily: "Manrope_700Bold",
+        },
     },
-    text4: {
-      fontSize: 10,
-      lineHeight: 14,
-      fontFamily: 'Manrope_700Bold',
-    },
-    text5: {
-      fontSize: 18,
-      lineHeight: 30,
-      fontFamily: 'Manrope_700Bold',
-    },
-    overline: {
-      fontSize: 14,
-      lineHeight: 19,
-      fontFamily: 'Manrope_700Bold',
-    },
-    linkText: {
-      textDecorationLine: 'underline',
-    },
-  },
 
-  textButton: {
-    fontSize: 12,
-    lineHeight: 16,
-    fontFamily: 'Manrope_700Bold',
-  },
+    textButton: {
+        fontSize: 12,
+        lineHeight: 16,
+        fontFamily: "Manrope_700Bold",
+    },
 
-  borderRadius: 8,
-  shadowProp: shadow,
+    borderRadius: 8,
+    shadowProp: shadow,
 
-  divider: {
-    marginVertical: 20,
-    width: '75%',
-    height: 1,
-  },
+    divider: {
+        marginVertical: 20,
+        width: "75%",
+        height: 1,
+    },
 };
 
 export default StyleGuide;
--- client/components/utils/icons/Brasser.jsx
+++ client/components/utils/icons/Brasser.jsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import * as React from "react";
 import Svg, { Path } from "react-native-svg";
 import StyleGuide from "../StyleGuide";
-=======
-import * as React from 'react';
-import Svg, { Path } from 'react-native-svg';
->>>>>>> origin
 
 const Brasser = (props) => {
     return (
