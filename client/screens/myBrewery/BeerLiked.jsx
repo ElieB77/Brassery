422ed094d712406303ea12cca47948b0ede41632
GauvainThery <gauvain.thery@payfit.com>
2022-03-10 10:18:01 +0100

    Fix beerLiked screen

--- client/screens/myBrewery/BeerLiked.jsx
+++ client/screens/myBrewery/BeerLiked.jsx
@@ -1,80 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
-import Header from '../../components/headings/Header';
-import StyleGuide from '../../components/utils/StyleGuide';
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import Header from "../../components/headings/Header";
+import StyleGuide from "../../components/utils/StyleGuide";
 
-import config from '../../config/globalVariables';
+import config from "../../config/globalVariables";
 
-import List from '../../components/lists/list';
-import ListItem from '../../components/lists/listItem';
+import List from "../../components/lists/list";
+import ListItem from "../../components/lists/listItem";
 
 const BeerLiked = ({ token, navigation }) => {
-  const [recipes, setRecipes] = useState([]);
-  const [recipesId, setRecipesId] = useState([]);
+    const [recipes, setRecipes] = useState([]);
+    const [recipesId, setRecipesId] = useState([]);
 
-  useEffect(() => {
-    async function getUser() {
-      const rawResponse = await fetch(`${config.base_url}/api/auth/me`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      const response = await rawResponse.json();
+    useEffect(() => {
+        async function getUser() {
+            const rawResponse = await fetch(`${config.base_url}/api/auth/me`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            const response = await rawResponse.json();
 
-      if (response.data) {
-        for (const id of response.data.likedRecipes) {
-          setRecipesId((prevState) => [...prevState, id]);
+            if (response.data) {
+                let temp = [...recipesId];
+                for (const id of response.data.likedRecipes) {
+                    temp.push(id);
+                }
+                setRecipesId(temp);
+            }
         }
-      }
-    }
-    getUser();
-  }, []);
+        getUser();
+    }, []);
 
-  useEffect(() => {
-    async function getRecipes() {
-      const newRawResponse = await fetch(`${config.base_url}/api/recipes`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+    useEffect(() => {
+        async function getRecipes() {
+            const newRawResponse = await fetch(
+                `${config.base_url}/api/recipes`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
 
-      const newResponse = await newRawResponse.json();
+            const newResponse = await newRawResponse.json();
 
-      for (const recipe of newResponse.data) {
-        if (recipesId.includes(recipe._id))
-          setRecipes((prevState) => [...prevState, recipe]);
-      }
-    }
-    getRecipes();
-  }, [recipesId]);
+            let temp = [...recipes];
+            for (const recipe of newResponse.data) {
+                if (recipesId.includes(recipe._id)) temp.push(recipe);
+            }
+            setRecipes(temp);
+        }
+        getRecipes();
+    }, [recipesId]);
 
-  return (
-    <View style={StyleGuide.container}>
-      <Header title='mes recettes likées' />
-      <List>
-        {recipes.map((item, index) => {
-          return (
-            <ListItem
-              key={index}
-              title={item.name}
-              content={item.description}
-              btnType='next'
-              onPress={() =>
-                navigation.navigate('Recipe', {
-                  recipeId: item._id,
-                })
-              }
-            />
-          );
-        })}
-      </List>
-    </View>
-  );
+    return (
+        <View style={StyleGuide.container}>
+            <Header title="mes recettes likées" />
+            <List>
+                {recipes.map((item, index) => {
+                    return (
+                        <ListItem
+                            key={index}
+                            title={item.name}
+                            content={item.description}
+                            btnType="next"
+                            onPress={() =>
+                                navigation.navigate("Recipe", {
+                                    recipeId: item._id,
+                                })
+                            }
+                        />
+                    );
+                })}
+            </List>
+        </View>
+    );
 };
 
 function mapStateToProps(state) {
-  return { token: state.token };
+    return { token: state.token };
 }
 
 export default connect(mapStateToProps, null)(BeerLiked);
